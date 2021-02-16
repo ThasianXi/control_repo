00872de40db6247051ee1aa2cb90765778f544cf
ThasianXi <ThasianXi@users.noreply.github.com>
2021-02-15 20:15:27 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,18 +1,22 @@
 class minecraft{
-file {'/opt/minecraft':
+$url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
+$install_dir = '/opt/minecraft'
+file {$install_dir:
   ensure => directory,
   }
   
-file {'/opt/minecraft/minecraft_server.jar':
+file {"${install_dir}/minecraft_server.jar":
   ensure => file,
-  source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar;,
+  source => $url,
+  before => Service['minecraft'],
  }
  
  package {'java':
   ensure => present,
+  
  }
  
- file {'/opt/minecraft/eula.txt':
+ file {"${install_dir}/eula.txt":
   ensure => file,
   content => 'eula=true',
  }
@@ -25,6 +29,7 @@ file {'/opt/minecraft/minecraft_server.jar':
  service {'minecraft':
   ensure=> running,
   enable => true,
+  require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
     }
     
    }
